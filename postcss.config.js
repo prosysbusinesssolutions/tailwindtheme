c3f031271ec00f54f3ed6090734c3abb81471b62
zohairguru <diasolarpower@gmail.com>
2021-02-17 22:12:02 -0800

    add purgecss and nano

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,27 @@
+const { plugin } = require("postcss");
+
+const plugins = [require("tailwindcss") , require("autoprefixer")];
+
+
+console.log(process.env.NODE_ENV);
+if(process.env.NODE_ENV = "production"){
+
+  const purgecss = require("@fullhuman/postcss-purgecss")({
+    content:["./dist/**/*.html"],
+    defaultExtractor:function(content){
+      console.log("content",content);
+      return content.match(/[A-Za-z0-9-_:/]+/g)||[];
+    }
+  });
+  plugins.push(purgecss);
+
+  const cssnano = require("cssnano")({
+    preset:"default"
+  });
+  plugins.push(cssnano);
+}
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: plugins
+  
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: ['./dist/**/*.html'],
+    },
+    
+ 
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     debugScreens: {
